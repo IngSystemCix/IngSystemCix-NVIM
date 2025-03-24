ed1713d59f731e4ac50f1e9ac98c45ec44b841f7
IngSystemCix <juanromerocollazos@gmail.com>
2025-03-24 03:51:34 -0500

    feat: fixed plugins

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -20,6 +20,7 @@ return {
         },
       },
       filetypes = { ["*"] = true },
+      copilot_node_command = vim.fn.executable("node") == 1 and "node" or nil,
       panel = { enabled = false }, -- Desactiva el panel flotante de Copilot
     },
     copilot_model = "gpt-4o-copilot",

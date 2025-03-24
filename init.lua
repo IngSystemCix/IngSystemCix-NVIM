a481a8daf0a935a81a9f0357165de019cf690a7c
IngSystemCix <juanromerocollazos@gmail.com>
2025-03-23 22:40:16 -0500

    feat: fixed plugins

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ vim.opt.termguicolors = true
 vim.cmd([[set termguicolors]])
 vim.notify("Welcome IngSystemCix", "info", { title = "Welcome" })
 
+vim.cmd [[ autocmd BufRead,BufNewFile *.graphql,*.gql set filetype=http ]]

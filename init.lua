af5477aed4cb76f6cc429f5a65e64cc9df419f9c
IngSystemCix <juanromerocollazos@gmail.com>
2025-05-15 15:39:31 -0500

    feat: spelling in es and en

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+vim.opt.spell = true
+vim.opt.spelllang = { "es", "en" }
 vim.cmd([[autocmd VimEnter * Screenkey]])
 vim.opt.termguicolors = true
 vim.cmd([[set termguicolors]])
